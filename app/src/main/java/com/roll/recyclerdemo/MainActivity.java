ed611e841f0483632fae837f47861d8d8465f1a6
eduardo <eduardosunahata@gmail.com>
2019-05-27 01:03:35 -0500

    Commit 3: - Cambios en MainActivity para mostrar los datos del recycler. - recycler_list: se cambia el height para que no ocupe toda la pantalla sino solo lo que ocupe.

--- app/src/main/java/com/roll/recyclerdemo/MainActivity.java
+++ app/src/main/java/com/roll/recyclerdemo/MainActivity.java
@@ -2,12 +2,43 @@ package com.roll.recyclerdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+
+    private ArrayList<String> textosArray;
+    private RecyclerView recyclerView;
+    private RecyclerViewAdapter recyclerAdapter;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initVars();
+    }
+
+    void initVars(){
+        textosArray = new ArrayList<>();
+        populateTextosArray();
+        initRecycler();
+    }
+
+    void initRecycler(){
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerAdapter = new RecyclerViewAdapter(this,textosArray);
+        recyclerView.setAdapter(recyclerAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    void populateTextosArray(){
+        for (int i=0;i<15;i++){
+            textosArray.add("Texto número "+i);
+        }
     }
 }
